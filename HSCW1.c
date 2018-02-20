03951ef4714d6467b4fd82a11d7fbb2387fdb1f9
Mark Schmieg <ms147@hw.ac.uk>
2018-02-20 13:45:29 +0000

    Pixels showing only 0 but it's something Pixels showing only 0 but it's something

--- HSCW1.c
+++ HSCW1.c
@@ -2,44 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+//linked list to hold comments
 struct LinkedList{
     char* comment;
     struct LinkedList * next;
 };
 
-
+//pixel structure
+typedef struct colours{
+	int Red, Green, Blue;
+}colours;
 
 
 //structure to hold a PPM image
 struct PPM{
     char magicNb[2];
-    struct LinkedList *commentValue;
+    struct LinkedList * commentValue;
     int width;
     int height;
     int max;
-    int *matrix;
+    colours * matrix;
 };
 
 struct PPM * getPPM(FILE * fd){
 
     struct PPM *image;
-    image = (struct PPM *) malloc (sizeof(struct PPM));
-    char com[100];
-    char ch;
-
-    struct LinkedList* head;
+    image = (struct PPM *) malloc (sizeof(struct PPM)); 
+    char com[100]; //string to store the comments which then gets passed as a value into the linked list
+    char ch; //char used for getting the next character
+	struct LinkedList* head;
     head = (struct LinkedList *)malloc(sizeof(struct LinkedList));
     head->comment = NULL;
     struct LinkedList *currentNode;
     currentNode = (struct LinkedList *)malloc(sizeof(struct LinkedList));
-    int counter;
-    int magicNb, width, height, max;
+	int counter; //counter for the array in which the pixels get stored in
+    int magicNb, width, height, max, r, g, b;
 
-    fscanf(fd,"%[^\n]", &(image->magicNb) );
+	//getting the PPM format P3
+    fscanf(fd,"%[^\n]", image->magicNb );
 	
-    ch = getc(fd); //to skip the new line after the magic number
+    ch = getc(fd); //to skip the new line after the PPM format
     if(strcmp((image->magicNb),"P3\n") !=0){
-
+   
     }else{
         printf("Wrong PPM format");
 		exit(0);
@@ -47,15 +51,13 @@ struct PPM * getPPM(FILE * fd){
 
     //getting the comments and storing them as nodes in a linked list.
     ch = getc(fd);
-
-
-    while(ch == '#'){
+	while(ch == '#'){
         fscanf(fd,"%[^\n]", com );
         if(head->comment == NULL){
             head->comment = com;
             head->next = NULL;
             currentNode = head;
-            printf("#%s\n", currentNode->comment);
+           // printf("#%s\n", currentNode->comment);
 
         }else{
             while (currentNode->next != NULL){
@@ -64,14 +66,14 @@ struct PPM * getPPM(FILE * fd){
 
             currentNode->next = (struct LinkedList *)malloc(sizeof(struct LinkedList));
             currentNode = currentNode->next;
-            //image->commentValue = head;
+            image->commentValue = head;
             currentNode->comment = malloc(sizeof(com));
             strcpy(currentNode->comment, com);
             currentNode->next = NULL;
-            printf("# %s\n", currentNode->comment);
+           // printf("# %s\n", currentNode->comment);
         }
 
-        //add(head, com); // adding a comment at the end of the linked list. THIS produces the segmaentation fault.
+       
         ch = getc(fd); //to skip the new line char.
         ch = getc(fd); //to read the first char of the next line.
 
@@ -82,45 +84,55 @@ struct PPM * getPPM(FILE * fd){
     //go back to the previous read char as it read the first digit of width.
     ungetc(ch, fd);
     fscanf(fd, "%d %d", &width, &height);
-
-    image->width=width;
+	image->width=width;
     image->height=height;
+	
     //getting the max value
     fscanf(fd, "%d", &max);
 	image->max = max;
 
-    image->matrix = malloc(3*(image->width*image->height)*sizeof(int));
-    for(counter=0; counter<3*(image->width*image->height); counter++){
-        fscanf(fd, "%d", &(image->matrix[counter]));
-        printf("%d\n", image->matrix[counter]);
+	//getting the pixels storing 3 bits into one colour struct which is then added to an array
+    image->matrix = (colours *) malloc(image->width * image->height * sizeof(colours));
+    for(counter=0; counter < image->width * image->height; counter++){
+        r= fscanf(fd, "%d", &image->matrix[counter].Red);
+		r= fscanf(fd, "%d", &image->matrix[counter].Green);
+		r = fscanf(fd, "%d", &image->matrix[counter].Blue);
+		//printf("%d %d %d \n", image->matrix[counter].Red, image->matrix[counter].Green, image->matrix[counter].Blue);
 
     }
 
+	//returning the ppm image
     return image;
 
 
 }
 
 void showPPM(struct PPM * i){
+	//printing the ppm format 
 	printf("P3\n");
     
-      struct LinkedList *currentNode = i->commentValue;
-      int n;
-      printf("%d\n", i->magicNb);
+	struct LinkedList *currentNode = i->commentValue;
+	int n;
+	//printing ppm format from ppm structure but it doesn't work
+	printf("%s\n", i->magicNb);
 
-      
-      while(currentNode != NULL){
-          printf("# %s\n", currentNode->comment);
-          currentNode = currentNode->next;
-      }
-  /*
+	//printing the comments but doesn't return anything
+	while(currentNode != NULL){
+	  printf("# %s\n", currentNode->comment);
+	  currentNode = currentNode->next;
+	}
+  
+	//printing width, height and max that is working 
     printf("%d %d\n", i->width, i->height);
-    printf("%d", i->max);
-    for(n=0;n<3*(i->width*i->height);i++){
-		printf("%d", i->matrix);
+    printf("%d\n", i->max);
+	
+	
+	//printing the bits of every pixel returns only 0s
+    for(n=0; n < i->width * i->height; n++){
+		printf("%d %d %d\n ", i->matrix[n].Red,  i->matrix[n].Green,  i->matrix[n].Blue );
 	}
 	
-*/
+
 
 }
 
